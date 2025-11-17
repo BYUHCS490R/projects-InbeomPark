f10b8797e104c1de98c3d4c5bfa69638dc88b49b
Inbeom Park <InbeomPark07@go.byuh.edu>
2025-11-17 11:50:50 -1000

    s

---
+++ activities/form/form.js
@@ -0,0 +1,24 @@
+
+        document.getElementById("myForm").addEventListener("submit",function(event){
+        event.preventDefault();
+
+        if (!name || !email) {
+            alert("You need to enter a name and email.");
+            return;
+        };
+
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "submit.json", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                document.getElementById("message").innerHTML = response.message;
+            } else if (xhr.readyState === 4) {
+                alert("Error submitting form.");
+            }
+        };
+        xhr.send(JSON.stringify(data));
+        console.log(data);
+        })
